769a0898998d2440e59b70a6dbff9ec202e3ff06
Jimmy Chao <daizenga@gmail.com>
2015-08-23 21:06:25 -0400

    Add mountElement

--- react.js
+++ react.js
@@ -33,21 +33,45 @@ var ReactCompositeComponent = function(element) {
 }
 
 ReactCompositeComponent.prototype.mountComponent = function() {
+  var instance = new this._currentElement.type()
+
+  var renderedElement = instance.render()
+
+  var renderedComponent = instantiateReactComponent(renderedElement)
+
+  return renderedComponent.mountComponent()
 }
 
 var ReactDOMComponent = function(element) {
   this._currentElement = element
 }
 
+ReactDOMComponent.prototype.mountComponent = function() {
+  var props = this._currentElement.props
+
+  var tagOpen = '<' + this._currentElement.type + '>'
+  var tagClose = '</' + this._currentElement.type + '>'
+
+  var renderedComponents = props.children.map(function(element){
+    return instantiateReactComponent(element)
+  })
+
+  var tagContent = renderedComponents.map(function(component) {
+    return component.mountComponent()
+  }).join('')
+
+  return tagOpen + tagContent + tagClose
+}
+
 function instantiateReactComponent(node) {
   if(typeof node === 'object') {
     if (typeof node.type === 'string') {
       return new ReactDOMComponent(node)
-    else if (typeof node.type === 'function')
+    } else if (typeof node.type === 'function') {
       return new ReactCompositeComponent()
     }
-  else if (typeof node === 'string' || typeof node === 'number') {
-    return new ReactTextComponent(node)
+  } else if (typeof node === 'string' || typeof node === 'number') {
+    return new ReactDOMTextComponent(node)
   }
 }
 
