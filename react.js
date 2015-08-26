3ed1bfbfbe8191ec26b0788ceb5bc23e7b0b07fe
Jimmy Chao <daizenga@gmail.com>
2015-08-26 10:19:49 -0400

    Step 2

--- react.js
+++ react.js
@@ -1,5 +1,20 @@
+var ReactElement = function(type, props) {
+  this.type = type
+  this.props = props
+}
+
+var createElement = function(type, config, children) {
+  var props = {}
+  for (propName in config) {
+    props[propName] = config[propName]
+  }
+  props.children = children
+
+  return new ReactElement(type, props)
+}
 
 var React = {
+  createElement: createElement,
 }
 
 if(typeof module !== 'undefined') {
